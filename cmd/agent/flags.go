9eafcbc22e49e11bcc991feecabfd304baf22f33
denis <denispavlov1989@gmail.com>
2025-03-04 11:57:55 +0700

    iter5

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"strconv"
 )
 
 var flagRunAddr string
@@ -13,4 +16,22 @@ func parseFlags() {
 	flag.IntVar(&flagReportInterval, "r", 10, "frequency of sending metrics to the server in seconds")
 	flag.IntVar(&flagPollInterval, "p", 2, "frequency of getting runtime metrics in seconds")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		flagRunAddr = envRunAddr
+	}
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		val, err := strconv.Atoi(envReportInterval)
+		if err != nil {
+			log.Fatal(err)
+		}
+		flagReportInterval = val
+	}
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		val, err := strconv.Atoi(envPollInterval)
+		if err != nil {
+			log.Fatal(err)
+		}
+		flagPollInterval = val
+	}
 }
